77d798d3fb9a8960ffc049c320b116f31ec67583
dhayatul <dohanum@gmail.com>
2024-06-28 07:04:09 +0000

    add button, slider, & input

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+
+house = pd.read_csv('house_clean.csv')
 
 def main() :
     st.header('Halaman Streamlit Dona')
@@ -6,6 +9,32 @@ def main() :
     st.markdown('# Rendering Markdown')
     st.write('Some Phytagoras Equation : ')
     st.latex('c^2 = a^2+b^2')
+
+    st.dataframe(house)
+
+    st.write('Metrics')
+    st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
+
+    st.write('Menampilkan Dataframe dengan St AgGrid')
+    st.table([x for x in range(1,5)])
+
+    click_me_btn = st.button('Click Me')
+    st.write(click_me_btn) #Return True kalo di Click 
+    check_btn = st.checkbox('Klik Jika Setuju')
+    if check_btn :
+        st.write('Anda Setuju')
+    
+    
+    radio_button= st.radio('Choose below',[x for x in range(1,3)])
+    st.write('Anda Memilih',radio_button)
+    
+    #slider 
+    age_slider = st.slider('Berapa Usia Anda',0,100)
+    st.write('Usia Anda',age_slider)
+    
+    #Input (Typing)
+    num_input = st.number_input('Input Berapapun')
+    st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
 
 if __name__ == '__main__':
     main()
